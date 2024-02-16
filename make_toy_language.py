e4ff7777ed8dc9611a52489f8391451e68314772
Hewson <hpdx3x@gmail.com>
2024-02-16 12:10:25 -0500

    add sigmoid synonym to toy language

--- make_toy_language.py
+++ make_toy_language.py
@@ -1,4 +1,5 @@
 ### Make toy language with markov property
+import math
 import random
 
 # make transition probabilities
@@ -30,12 +31,25 @@ pr[bos_word] = get_probs()
 for word in words:
     pr[word] = get_probs()
 
-### Generate 'texts'
-def generate_k_texts(k = 1000, text_length = 100):
+# TBD: add Gaussian noise?
+
+### Generate k years of 'text'
+def generate_k_texts(k = 50, text_length = 10000, combo=False, combo_word = 'F', combo_freq = 0.4):
 
     texts = []
 
-    for _ in range(k):
+    combo_words = []
+    if combo:
+        combo_words = random.sample(words, k = 2)
+        print("Adding combination of", combo_words, "as", combo_word)
+
+        # set shift params
+        midpoint = random.choice(range(k))
+        s = random.uniform(0, k/2) / k
+
+        print("shift parameters: m =", midpoint, "& s = ", s)
+
+    for year in range(k):
 
         t = []
         cur = bos_word
@@ -47,10 +61,34 @@ def generate_k_texts(k = 1000, text_length = 100):
             t.append(next)
             cur = next
 
+        # substitute combined word in if needed
+        if combo:
+
+            # shift = \sigma( s * (t - m) )
+            shift = 1.0 / (1 + math.exp(-(s * (year - midpoint))))
+            shift_fst = shift * combo_freq
+            shift_scd = (1 - shift) * combo_freq
+
+            for i in range(len(t)):
+
+                w_i = t[i]
+                sub_prob = random.random()
+
+                should_subst_first = w_i == combo_words[0] and sub_prob < shift_fst
+                should_subst_second = w_i == combo_words[1] and sub_prob < shift_scd
+
+                if should_subst_first or should_subst_second:
+
+                    t[i] = combo_word
+            
+
         texts.append(" ".join(t))
 
     return texts
 
-ts = generate_k_texts(5, 20)
+ts = generate_k_texts(combo=True)
 
-print(ts, pr['a'])
+# show probabilities
+print("probabilities:")
+for word in words:
+    print(word, "::", pr[word])
